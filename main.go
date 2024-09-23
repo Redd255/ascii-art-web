21292f9fd96fe02152abd98dc6e2bdcdbfb22f52
yhayyani <youssef16hay@gmail.com>
2024-09-23 15:11:54 +0100

    ascii art web

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"asciiart/serv"
-	"log"
-	"net/http"
+	"asciiart/serv"  
+	"log"            
+	"net/http"      
 )
 
 func main() {
+	// Handle the root URL path ("/") by mapping it to the Index function in the serv package
+	// This will serve as the homepage for the application
 	http.HandleFunc("/", serv.Index)
-	http.HandleFunc("/ascii-art", serv.AsciiWeb)
-	log.Println("http//localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 
+	// Handle the "/ascii-art" URL path by mapping it to the AsciiWeb function in the serv package
+	// This endpoint will likely handle the ASCII art generation and serve the related output
+	http.HandleFunc("/ascii-art", serv.AsciiWeb)
+
+	// Log a message to the console to inform that the server is running and accessible at localhost:8080
+	log.Println("Server running at http://localhost:8080")
+
+	// Start the HTTP server on port 8080 and listen for incoming requests
+	// If the server encounters an error, ListenAndServe will return it
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err) // Log fatal error if the server fails
+	}
 }
--- serv/server.go
+++ serv/server.go
@@ -6,10 +6,11 @@ import (
 	"net/http"
 	"strings"
 )
-
+// global variable containing the template
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	// if method not "GET", will return Only GET method is allowed
 	if r.Method != "GET" {
 		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
 		return
@@ -18,44 +19,39 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	// execute the template with nil data
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
+
 func AsciiWeb(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Failed to parse form", http.StatusBadRequest)
 		return
 	}
+	// if method not "POST", will return Only POST method is allowed
 	if r.Method != "POST" {
 		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	//get the text from the request
 	textInput := asciiart.CheckInput(r.FormValue("text"))
 	textLines := strings.Split(textInput, "\r\n")
+	//get the banner from the request
 	banner := r.FormValue("banner")
 	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
-		maps, err := asciiart.MapBanner("standard")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		asciiArt := asciiart.Draw(maps, textLines)
-		err = tmpl.Execute(w, asciiArt)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		return
+		banner = "standard"
 	}
 	maps, err := asciiart.MapBanner(banner)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	// draw the ascii art and execute the template with the result
 	asciiArt := asciiart.Draw(maps, textLines)
 	err = tmpl.Execute(w, asciiArt)
 	if err != nil {
